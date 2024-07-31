db8f2021a45f41e679632ef7528b0566a3890b45
divya034 <59786992+divya034@users.noreply.github.com>
2024-07-31 13:53:34 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,7 +1,7 @@
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t rishabh9593776/dockerflask-1.0 .'
+        sh 'docker build -t divya9593776/dockerflask-1.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh 'docker push divya9593776/dockerflask-1.0'
     }
